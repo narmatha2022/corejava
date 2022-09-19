2da031f1e7201711a8f126e9b0d3398ac5b21c80
narmatha <narmathajayakumar2021@gmail.com>
2022-09-19 12:02:29 -0700

    adding code

---
+++ src/NarmathaAssignment1/Student.java
@@ -0,0 +1,17 @@
+package NarmathaAssignment1;
+//Create a class "Student.java" with a variable "Name". Write two functions:
+//getName() that returns "Name" from state.
+//setName() that takes an argument and updates the state variable "Name".
+public class Student {
+    public String stateName;
+    public String getStateName() {
+        return stateName ;
+    }
+    public void  setStateName(String stateName){
+        this.stateName=stateName;
+
+
+    }
+
+    }
+
---
+++ src/NarmathaAssignment1/Tester.java
@@ -0,0 +1,10 @@
+package NarmathaAssignment1;
+
+public class Tester {
+    public static void main (String[] args){
+        Student studentObj=new Student();
+        studentObj.setStateName("California");
+        System.out.println("The State Name is " + studentObj.getStateName());
+
+    }
+}
